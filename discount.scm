dda0d77853d498eb5a74038c1cff427c3d5cf7f6
David Banks <amoebae@gmail.com>
2010-12-30 14:25:15 +0000

    removing extraneous demo data; now imports

--- discount.scm
+++ discount.scm
@@ -1,19 +1,13 @@
 #!r6rs
 
-(import (rnrs)
-        (mosh ffi)
-        (ffi-streams))
+(library (discount)
+  (export mkd-in)
+  (import (rnrs)
+          (mosh ffi))
 
-(define (say obj)
-  (display obj)
-  (newline))
+; FIXME: filename should be determined by libtool / configure.ac
+(define lib (open-shared-library "libmarkdown.so"))
 
-(let* ((lib (open-shared-library "libmarkdown.so"))
-       (mkd-in (c-function lib void* mkd_in void* int))
-       (markdown (c-function lib int markdown void* void* int)))
-  (let* ((in-stream (fopen "/home/amoe/test.txt" "r"))
-         (out-stream (fopen "/home/amoe/test.html" "w"))
-         (mmiot (mkd-in in-stream 0)))
-    (markdown mmiot out-stream 0)
-    (fclose in-stream)
-    (fclose out-stream)))
+(define mkd-in (c-function lib void* mkd_in void* int))
+
+)
