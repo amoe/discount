086517a0809b0f9fd829cb112228050c04cfd232
David Banks <amoebae@gmail.com>
2011-02-07 15:05:34 +0000

    adding many functions and preprocessor constants

--- discount.scm
+++ discount.scm
@@ -6,7 +6,35 @@
           mkd-string
           mkd-compile
           mkd-css
-          mkd-generatecss)
+          mkd-generatecss
+          mkd-document
+          mkd-generatehtml
+          mkd-xhtmlpage
+          mkd-toc
+
+          ; preprocessor constants
+          *nolinks*
+          *noimage*
+          *nopants*
+          *nohtml*
+          *strict*
+          *tagtext*
+          *no_ext*
+          *cdata*
+          *nosuperscript*
+          *norelaxed*
+          *notables*
+          *nostrikethrough*
+          *toc*
+          *1_compat*
+          *autolink*
+          *safelink*
+          *noheader*
+          *tabstop*
+          *nodivquote*
+          *noalphalist*
+          *nodlist*
+          *embed*)
   (import (rnrs)
           (mosh ffi))
 
@@ -19,6 +47,33 @@
 (define mkd-compile (c-function lib int mkd_compile void* int))
 (define mkd-css (c-function lib int mkd_css void* void*))
 (define mkd-generatecss (c-function lib int mkd_generatecss void* void*))
+(define mkd-document (c-function lib int mkd_document void* void*))
+(define mkd-generatehtml (c-function lib int mkd_generatehtml void* void*))
+(define mkd-xhtmlpage (c-function lib int mkd_xhtmlpage void* int void*))
+(define mkd-toc (c-function lib int mkd_toc void* void*))
+
+(define *nolinks* 1)
+(define *noimage* 2)
+(define *nopants* 4)
+(define *nohtml* 8)
+(define *strict* 16)
+(define *tagtext* 32)
+(define *no_ext* 64)
+(define *cdata* 128)
+(define *nosuperscript* 256)
+(define *norelaxed* 512)
+(define *notables* 1024)
+(define *nostrikethrough* 2048)
+(define *toc* 4096)
+(define *1_compat* 8192)
+(define *autolink* 16384)
+(define *safelink* 32768)
+(define *noheader* 65536)
+(define *tabstop* 131072)
+(define *nodivquote* 262144)
+(define *noalphalist* 524288)
+(define *nodlist* 1048576)
+(define *embed* 35)
 
 )
  
--- test.scm
+++ test.scm
@@ -1,12 +1,20 @@
 (import (rnrs)
+        (prefix (srfi :13) srfi-13:)
+        (mosh)
         (mosh test)
         (mosh ffi)
         (discount)
         (stdio))
 
 (define *style-header* "<style> ul { display: none; } </style>\n")
+(define *heading-text* "HEADING")
+(define *heading-markup* "\n--------\n")
 (define *body* "hello world")
-(define *test-string* (string-append *style-header* *body*))
+(define *test-string*
+  (string-append *style-header*
+                 *heading-text*
+                 *heading-markup*
+                 *body*))
 (define *test-size*   (string-length *test-string*))
 
 (let ((in-stream (tmpfile))
@@ -53,13 +61,50 @@
       (let ((real-ptr (pointer-ref-c-pointer ptr 0)))
         (test-equal *style-header* (extract-string real-ptr size))))))
 
-        
+; This plus mkd-generatehtml test below should do more validation on what ends
+; up being written to output stream.  At the very least we should count the
+; length of what was written to out-stream and make sure it's reasonably sized.
+       
 (let ((mmiot (mkd-string *test-string* *test-size* 0))
       (out-stream (tmpfile)))
   (mkd-compile mmiot 0)
-  (test-false   ; makeshift "does not throw exception"
+  (test-true   ; makeshift "does not throw exception"
     (begin
       (mkd-generatecss mmiot out-stream)
-      #f)))
+      #t)))
+
+(let ((mmiot (mkd-string *test-string* *test-size* 0)))
+  (mkd-compile mmiot 0)
+  (let ((ptr (malloc size-of-pointer)))
+    (let ((size (mkd-document mmiot ptr)))
+      (let ((real-ptr (pointer-ref-c-pointer ptr 0)))
+        (let ((ret (extract-string real-ptr size)))
+          (test-true (srfi-13:string-contains ret *body*))
+          (test-true (>= size (string-length *body*))))))))
+
+(let ((mmiot (mkd-string *test-string* *test-size* 0))
+      (out-stream (tmpfile)))
+  (mkd-compile mmiot 0)
+  (test-true   ; makeshift "does not throw exception"
+    (begin
+      (mkd-generatehtml mmiot out-stream)
+      #t)))
+
+(let ((mmiot (mkd-string *test-string* *test-size* 0))
+      (out-stream (tmpfile)))
+  (mkd-compile mmiot 0)
+  (test-true   ; makeshift "does not throw exception"
+    (begin
+      (mkd-xhtmlpage mmiot 0 out-stream)
+      #t)))
+
+(let ((mmiot (mkd-string *test-string* *test-size* 0)))
+  (mkd-compile mmiot *toc*)
+  (let ((ptr (malloc size-of-pointer)))
+    (let ((size (mkd-toc mmiot ptr)))
+      (let ((real-ptr (pointer-ref-c-pointer ptr 0)))
+        (let ((ret (extract-string real-ptr size)))
+          (test-true (srfi-13:string-contains ret *heading-text*))
+          (test-true (>= size (string-length *heading-text*))))))))
 
 (test-results)
