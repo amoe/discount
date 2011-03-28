5e7355dc111d5c1498c725320ed7a3ff81dda314
David Banks <amoebae@gmail.com>
2011-03-28 10:40:44 +0100

    simple string->string function

--- markdown.scm
+++ markdown.scm
@@ -4,11 +4,28 @@
   (export markdown)
   (import (rnrs)
           (prefix (discount) discount:)
-          (xitomatl keywords))
+          (xitomatl keywords)
+          (mosh ffi))
 
   (define/kw (markdown source
                        (format :default 'html)
                        (stream :default #t)
                        (properties :default (list)))
-    (values 'stub:document-object
-            'stub:render-value)))
+    (let ((mmiot (discount:mkd-string source (string-length source) 0))
+          (result (malloc size-of-pointer)))
+      (discount:mkd-compile mmiot 0)
+      (let* ((size (discount:mkd-document mmiot result))
+             (real-ptr (pointer-ref-c-pointer result 0))
+             (str-result (extract-string real-ptr size)))
+        (free result)
+        str-result)))
+        
+  ; FIXME: Optimize.
+  (define (extract-string ptr size)
+    (list->string
+     (let loop ((n 0))
+       (if (>= n size)
+           '()
+           (cons (integer->char (pointer-ref-c-signed-char ptr n))
+                 (loop (+ n 1)))))))
+)
